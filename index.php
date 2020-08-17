8e6fede9798357829e3c7169af7395653ce20457
VirtualTeam18 <Virtual.Team.Oskar@gmail.com>
2020-08-17 11:43:17 +0200

    Glowa

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE HTML>
+
 <html lang="pl">
 	<head>
 		<title>Elitars</title>
 		<meta charset="utf-8"/>
+		<meta name="description" content="?" />
+		<meta name="keywords" content="?" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	</head>
-
 	<body>
-	
+		<b>Test</b>
 	</body>
 </html>
