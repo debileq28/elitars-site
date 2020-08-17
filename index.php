baf97a5d98bb30d4744df45334e7533e12099c73
VirtualTeam18 <Virtual.Team.Oskar@gmail.com>
2020-08-17 11:38:54 +0200

    tabulator

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML>
 <html lang="pl">
-<head>
-	<meta charset="utf-8"/>
-	<title>Elitars</title>
-</head>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Elitars</title>
+	</head>
 
-<body>
+	<body>
 	
-</body>
+	</body>
 </html>
