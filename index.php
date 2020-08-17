9a08b0c6e53addda2546b586510052ee6ad5cc58
VirtualTeam18 <Virtual.Team.Oskar@gmail.com>
2020-08-17 11:50:40 +0200

    php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
 		<title>Elitars</title>
 		<meta charset="utf-8"/>
 		<meta name="description" content='Strona główna serwera minecraft "elitars.ml". Na stronie znajduje się regulamin serwera jak i dużo innych ciekawych reczy na temat serwera.' />
-		<meta name="keywords" content="elitars, minecraft, elitars.ml, server, server minecraft, fajne serwery minecraft, 1.16, 1.16.1" />
+		<meta name="keywords" content="elitars, minecraft, elitars.ml, server, server minecraft, fajne serwery minecraft, 1.16, 1.16.1, 1.16.2" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	</head>
 	<body>
-		<b>Test</b>
+		<?
+			echo Siemka!
+		?>
 	</body>
 </html>
