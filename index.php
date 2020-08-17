a83fbaa5e21f0149ee5afcbff4ffbed6ca860287
VirtualTeam18 <Virtual.Team.Oskar@gmail.com>
2020-08-17 11:54:58 +0200

    autor

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<head>
 		<title>Elitars</title>
 		<meta charset="utf-8"/>
+		<meta name="author" content="Oskar Pietrzko & Piotrek Szczepański" />
 		<meta name="description" content='Strona główna serwera minecraft "elitars.ml". Na stronie znajduje się regulamin serwera jak i dużo innych ciekawych reczy na temat serwera.' />
 		<meta name="keywords" content="elitars, minecraft, elitars.ml, server, server minecraft, fajne serwery minecraft, 1.16, 1.16.1, 1.16.2" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
