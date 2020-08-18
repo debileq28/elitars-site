32046e76f6ecaa806fec93dc062897dd01347f32
debileq28 <mjanowicie@gmail.com>
2020-08-18 22:27:35 +0200

    ZMIANY

--- Podstrony/autorzy.php
+++ Podstrony/autorzy.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<script data-ad-client="ca-pub-1031473336447998" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<link rel="stylesheet" href="../css/index.css" type="text/css" />
+		<link rel="stylesheet" href="../css/Autorzy.css" type="text/css" />
 		<link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
 	</head>
 	<body>
@@ -15,10 +15,10 @@
 			</div>
 			<div id="menu">
 				<a href="../index.php"><div class="option" id="Main">Strona główna</div></a>
-				<a href="../Podstrony/regulamin.php"><div class="option" id="Regulamin">Regulamin</div></a>
-				<a href="../Podstrony/topka.php"><div class="option" id="Top10">Top 10 graczy</div></a>
-				<a href="../Podstrony/zdjecia.php"><div class="option" id="Zdjecia">Zdjęcia</div></a>
-				<a href="../Podstrony/autorzy.php"><div class="option" id="Author">Autorzy</div></a>
+				<a href="../Podstrony/Regulamin.php"><div class="option" id="Regulamin">Regulamin</div></a>
+				<a href="../Podstrony/Topka.php"><div class="option" id="Top10">Top 10 graczy</div></a>
+				<a href="../Podstrony/Zdjecia.php"><div class="option" id="Zdjecia">Zdjęcia</div></a>
+				<a href="../Podstrony/Autorzy.php"><div class="option" id="Author">Autorzy</div></a>
 				<div style="clear:both;"></div>
 			</div>
 			<div id="topbar">
@@ -33,10 +33,10 @@
 			</div>
 			<div id="sidebar">
 				<a href="../index.php"><div class="optionL" id="Main">Strona główna</div></a>
-				<a href="../Podstrony/regulamin.php"><div class="optionL" id="Regulamin">Regulamin</div></a>
-				<a href="../Podstrony/topka.php"><div class="optionL" id="Top10">Top 10 graczy</div></a>
-				<a href="../Podstrony/zdjecia.php"><div class="optionL" id="Zdjecia">Zdjęcia</div></a>
-				<a href="../Podstrony/autorzy.php"><div class="optionL" id="Author">Autorzy</div></a>
+				<a href="../Podstrony/Regulamin.php"><div class="optionL" id="Regulamin">Regulamin</div></a>
+				<a href="../Podstrony/Topka.php"><div class="optionL" id="Top10">Top 10 graczy</div></a>
+				<a href="../Podstrony/Zdjecia.php"><div class="optionL" id="Zdjecia">Zdjęcia</div></a>
+				<a href="../Podstrony/Autorzy.php"><div class="optionL" id="Author">Autorzy</div></a>
 			</div>
 			<div id="content">
 				
